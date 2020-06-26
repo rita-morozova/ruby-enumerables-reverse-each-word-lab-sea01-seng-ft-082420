c5d06f69d42f2fdb6a7b759b3de95232acafff04
Margarita Morozova <ritamorozova@yahoo.com>
2020-06-26 02:07:53 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_each_word(sentence)
+   array = sentence.split(" ")
+   new_array = []
+   array.collect {|word| word.reverse}
+      array.each do |word|
+      new_array << word.reverse
+   end 
+   new_array.join(" ")
+end 
+ 
+
+  
+   
+   
